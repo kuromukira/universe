4bb873df020af1a983583c52f60d4c221f2144db
Nor Gelera <norgelera@outlook.com>
2022-10-11 12:54:14 +0800

    Fix issue with nested column name as sql parameters

--- code/Universe/Options/Parameters.cs
+++ code/Universe/Options/Parameters.cs
@@ -1,14 +1,32 @@
-﻿namespace Universe.Options.Query;
+﻿using System.Text.RegularExpressions;
+
+namespace Universe.Options.Query;
 
 /// <summary>
 /// Create a generic query string from a list of parameters. The first parameter created will be the first parameter in the where clause of the query string.
 /// </summary>
-/// <param name="Column">Column name. Excluding the @ symbol for SQL parameters</param>
+/// <param name="Column">Column name</param>
 /// <param name="Value">Value for the where clause associated with the Column</param>
+/// <param name="Alias">
+///     Column name alias excluding the @ symbol for SQL Parameters.
+///     If not supplied, this will use the value from <paramref name="Column"/>
+/// </param>
 /// <param name="Where">Where operator (eg AND / OR)</param>
 /// <param name="Operator">Boolean expression operator</param>
 public record struct Catalyst(
     string Column,
     object Value,
+    string Alias,
     Q.Where Where = Q.Where.And,
     Q.Operator Operator = Q.Operator.Eq);
+
+public static class CatalystExtension
+{
+    public static string ParameterName(this Catalyst catalyst)
+    {
+        if (string.IsNullOrWhiteSpace(catalyst.Alias))
+            return Regex.Replace(catalyst.Column, "[^\\w\\d]", string.Empty);
+
+        return catalyst.Alias;
+    }
+}
