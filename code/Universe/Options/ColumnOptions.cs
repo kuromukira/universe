8546cebc01a53825de23367fd58383472a23ddd6
nor <9804927+kuromukira@users.noreply.github.com>
2022-10-11 14:28:21 +0800

    Implement column options (#12)
    * add group by and scaffold for new column options
    
    * implement new column options
    
    * rename column options class
    
    * fix borken link

---
+++ code/Universe/Options/ColumnOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace Universe.Options.Query;
+
+/// <summary></summary>
+/// <param name="Names">List of column names to be part of the query</param>
+/// <param name="IsDistinct">Adds DISTINCT in the generated query</param>
+/// <param name="Top">Only select the specified number of top rows</param>
+/// <param name="Count">
+///     Count the number of rows. Please note that the column <paramref name="Names"/>
+///     will be added as part of the GROUP BY clause
+/// </param>
+public record struct ColumnOptions(
+    IList<string> Names,
+    bool IsDistinct = false,
+    int Top = 0,
+    bool Count = false);
